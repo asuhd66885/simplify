7498a8f469ecd6844a69e63924e5aa274516465b
asuhd66885 <ray3bow1@gmail.com>
2022-12-17 23:24:58 +0900

    ユーザー管理機能完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+  before_action :authenticate_user!, except: [:index]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '2222'
+    end
+  end
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root to: "stuffs#index"
 
-  get 'stuffs/index'
-root to: "stuffs#index"
+  resources :users, only: [:edit, :update]
 
 end
